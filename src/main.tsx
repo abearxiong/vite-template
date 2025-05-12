263fba960005c45c22684cf0d79bbe1e216ef202
abearxiong <xiongxiao1012@outlook.com>
2025-05-12 14:00:46 +0800

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { App, AppRoute } from './pages/App.tsx';
-import { CustomThemeProvider } from '@kevisual/components/theme/index.tsx';
+// import { CustomThemeProvider } from '@kevisual/components/theme/index.tsx';
 
-console.log('cu',)
-createRoot(document.getElementById('root')!).render(
-  <CustomThemeProvider>
-    <AppRoute />
-  </CustomThemeProvider>,
-);
+console.log('cu');
+// createRoot(document.getElementById('root')!).render(
+//   <CustomThemeProvider>
+//     <AppRoute />
+//   </CustomThemeProvider>,
+// );
+
+createRoot(document.getElementById('root')!).render(<App />);

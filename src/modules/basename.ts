3b6047803663f494675e0514024582cc02f49ba8
abearxiong <xiongxiao1012@outlook.com>
2026-02-04 17:59:42 +0800

    update

--- src/modules/basename.ts
+++ src/modules/basename.ts
@@ -1,1 +1,2 @@
+// @ts-ignore
 export const basename = DEV_SERVER ? '/' : BASE_NAME;
---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import pkgs from './package.json';
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+const isDev = process.env.NODE_ENV === 'development';
+const basename = isDev ? '/' : pkgs?.basename || '/';
+
+let target = process.env.VITE_API_URL || 'http://localhost:51515';
+const apiProxy = { target: target, changeOrigin: true, ws: true, rewriteWsOrigin: true, secure: false, cookieDomainRewrite: 'localhost' };
+let proxy = {
+  '/root/': apiProxy,
+  '/api': apiProxy,
+  '/client': apiProxy,
+};
+/**
+ * @see https://vitejs.dev/config/
+ */
+export default defineConfig({
+  plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: basename,
+  define: {
+    BASE_NAME: JSON.stringify(basename),
+  },
+  server: {
+    port: 7008,
+    host: '0.0.0.0',
+    allowedHosts: true,
+    proxy,
+  },
+});
